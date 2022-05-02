8becced416700791920c0e640e9d4bd4475f7f32
DMTRY Z <toafriend27@gmail.com>
2022-05-02 10:25:32 +0300

    fix: add fonts and js fix

--- script/script.js
+++ script/script.js
@@ -1,37 +1,6 @@
 let popup = qs('.popup');
 let popupOpenBtn = qs('.profile__edit-button');
 let popupClostBtn = qs('.popup__close-button');
-
-function qs(selector) {
-    return document.querySelector(selector);
-}
-
-function togglePopup(popUpObj) {
-    popUpObj.classList.toggle('popup_opened');
-}
-
-popupOpenBtn.addEventListener('click', function () {
-    togglePopup(popup);
-    // popup.classList.toggle('popup_opened');
-});
-
-popupClostBtn.addEventListener('click', function (event) {
-    // popup.classList.toggle('popup_opened');
-    // либо
-    // let currentPopup = event.target.closest('.popup');
-    // currentPopup.classList.toggle('popup_opened');
-    // либо
-    let currentPopup = event.target.closest('.popup').classList.toggle('popup_opened');
-});
-
-popup.addEventListener('click', function (event) {
-    if (event.target == event.currentTarget && popup.classList.toggle('popup_opened')) {
-        togglePopup(popup);
-    }
-});
-
-// Нижа часть за редактирование имени и информации о себе
-
 let formElement = qs('.popup__container');
 
 let defaultName = qs('.profile__name');
@@ -40,6 +9,15 @@ let defaultPassion = qs('.profile__passion');
 let nameInput = qs('.popup__name-input');
 let passionInput = qs('.popup__passion-input');
 
+function qs(selector) {
+    return document.querySelector(selector);
+}
+
+function togglePopup(popUpObj) {
+    popUpObj.classList.toggle('popup_opened');
+    nameInput.value = defaultName.textContent;
+    passionInput.value = defaultPassion.textContent;
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -47,6 +25,27 @@ function formSubmitHandler(evt) {
     defaultPassion.textContent = passionInput.value;
     togglePopup(popup);
 }
+
+popupOpenBtn.addEventListener('click', function () {
+    togglePopup(popup);
+    // popup.classList.toggle('popup_opened');
+});
+
+popupClostBtn.addEventListener('click', function (event) {
+    togglePopup(popup);
+    // popup.classList.toggle('popup_opened');
+    // либо
+    // let currentPopup = event.target.closest('.popup');
+    // currentPopup.classList.toggle('popup_opened');
+    // либо
+    // let currentPopup = event.target.closest('.popup').classList.toggle('popup_opened');
+});
+
+popup.addEventListener('click', function (event) {
+    if (event.target == event.currentTarget && popup.classList.toggle('popup_opened')) {
+        togglePopup(popup);
+    }
+});
 
 formElement.addEventListener('submit', formSubmitHandler);
 
