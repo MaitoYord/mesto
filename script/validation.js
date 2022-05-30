21b88e6d551b361ae86dd56e1baf3717a3182cd4
DMTRY Z <toafriend27@gmail.com>
2022-05-30 20:37:39 +0300

    feat: start validation for js

---
+++ script/validation.js
@@ -0,0 +1,8 @@
+const config = {
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__submit',
+    inactiveButtonClass: 'form__submit_inactive',
+    inputErrorClass: 'popup__input_type_err',
+    errorClass: 'popup__err_active'
+}
